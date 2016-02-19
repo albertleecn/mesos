d34ffc864fd7077a218a05ceccba01e39f48a20c
Alex Barreto <shakamunyi@gmail.com>
2016-02-19 15:11:57 -0800

    2016-02-19:15:11:54

--- src/slave/containerizer/mesos/provisioner/docker/store.cpp
+++ src/slave/containerizer/mesos/provisioner/docker/store.cpp
@@ -169,9 +169,10 @@ Future<ImageInfo> StoreProcess::get(const mesos::Image& image)
 
   Try<spec::ImageReference> reference =
     spec::parseImageReference(image.docker().name());
+
   if (reference.isError()) {
     return Failure("Failed to parse docker image '" + image.docker().name() +
-                   "' as 'reference': " + reference.error());
+                   "': " + reference.error());
   }
 
   return metadataManager->get(reference.get())
@@ -184,6 +185,12 @@ Future<Image> StoreProcess::_get(
     const spec::ImageReference& reference,
     const Option<Image>& image)
 {
+  // NOTE: Here, we assume that image layers are not removed without
+  // first removing the metadata in the metadata manager first.
+  // Otherwise, the image we return here might miss some layers. At
+  // the time we introduce cache eviction, we also want to avoid the
+  // situation where a layer was returned to the provisioner but is
+  // later evicted.
   if (image.isSome()) {
     return image.get();
   }
