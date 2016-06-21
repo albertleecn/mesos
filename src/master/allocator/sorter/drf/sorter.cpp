7f2f269eef17d23abda53f53d3be5464758b3f96
Alex Barreto <shakamunyi@gmail.com>
2016-06-21 16:10:16 -0700

    2016-06-21:16:10:13

--- src/master/allocator/sorter/drf/sorter.cpp
+++ src/master/allocator/sorter/drf/sorter.cpp
@@ -14,6 +14,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <algorithm>
+#include <list>
+#include <set>
+#include <string>
+
+#include <mesos/mesos.hpp>
+#include <mesos/resources.hpp>
+
+#include <process/pid.hpp>
+
+#include <stout/check.hpp>
+#include <stout/foreach.hpp>
+#include <stout/option.hpp>
+
 #include "logging/logging.hpp"
 
 #include "master/allocator/sorter/drf/sorter.hpp"
@@ -21,6 +35,8 @@
 using std::list;
 using std::set;
 using std::string;
+
+using process::UPID;
 
 namespace mesos {
 namespace internal {
@@ -40,8 +56,8 @@ bool DRFComparator::operator()(const Client& client1, const Client& client2)
 
 
 DRFSorter::DRFSorter(
-    const process::UPID& allocator,
-    const std::string& metricsPrefix)
+    const UPID& allocator,
+    const string& metricsPrefix)
   : metrics(Metrics(allocator, *this, metricsPrefix)) {}
 
 
@@ -65,6 +81,13 @@ void DRFSorter::update(const string& name, double weight)
 {
   CHECK(weights.contains(name));
   weights[name] = weight;
+
+  // If the total resources have changed, we're going to
+  // recalculate all the shares, so don't bother just
+  // updating this client.
+  if (!dirty) {
+    update(name);
+  }
 }
 
 
@@ -331,6 +354,10 @@ list<string> DRFSorter::sort()
     }
 
     clients = temp;
+
+    // Reset dirty to false so as not to re-calculate *all*
+    // shares unless another dirtying operation occurs.
+    dirty = false;
   }
 
   list<string> result;
